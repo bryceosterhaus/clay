1492b1fa0a840eab00b67e31b365d5c94e3af39e
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2020-04-14 14:33:23 -0700

    will squash before merge:

--- packages/clay-list/src/index.tsx
+++ packages/clay-list/src/index.tsx
@@ -1,11 +1,6 @@
 /**
- * SPDX-FileCopyrightText: © 2019 Liferay, Inc. <https://liferay.com>
+ * SPDX-FileCopyrightText: © 2020 Liferay, Inc. <https://liferay.com>
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import ClayList from './List';
-import {ClayListWithItems} from './ListWithItems';
-
-export {ClayListWithItems};
-
-export default ClayList;
+export default () => {};
--- packages/clay-pagination-bar/src/index.tsx
+++ packages/clay-pagination-bar/src/index.tsx
@@ -1,11 +1,6 @@
 /**
- * SPDX-FileCopyrightText: © 2019 Liferay, Inc. <https://liferay.com>
+ * SPDX-FileCopyrightText: © 2020 Liferay, Inc. <https://liferay.com>
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import ClayPaginationBar from './PaginationBar';
-import {ClayPaginationBarWithBasicItems} from './PaginationBarWithBasicItems';
-
-export {ClayPaginationBarWithBasicItems};
-
-export default ClayPaginationBar;
+export default () => {};
