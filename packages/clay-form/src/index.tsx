5d4859166745bed3e653e74ff6e3155c314d2f97
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2020-04-14 14:24:50 -0700

    will squash before merge:

--- packages/clay-form/src/index.tsx
+++ packages/clay-form/src/index.tsx
@@ -1,29 +1,6 @@
 /**
- * SPDX-FileCopyrightText: © 2019 Liferay, Inc. <https://liferay.com>
+ * SPDX-FileCopyrightText: © 2020 Liferay, Inc. <https://liferay.com>
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import ClayCheckbox from './Checkbox';
-import ClayDualListBox from './DualListBox';
-import ClayForm from './Form';
-import ClayInput from './Input';
-import ClayRadio from './Radio';
-import ClayRadioGroup from './RadioGroup';
-import ClaySelect from './Select';
-import ClaySelectBox from './SelectBox';
-import ClaySelectWithOption from './SelectWithOption';
-import ClayToggle from './Toggle';
-
-export {
-	ClayCheckbox,
-	ClayInput,
-	ClayDualListBox,
-	ClayRadio,
-	ClayRadioGroup,
-	ClaySelect,
-	ClaySelectBox,
-	ClaySelectWithOption,
-	ClayToggle,
-};
-
-export default ClayForm;
+export default () => {};
