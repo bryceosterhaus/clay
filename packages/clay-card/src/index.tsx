6291ef84a35fc34545e0f22d23b1eb1ba39e9ef2
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2020-04-14 14:25:12 -0700

    will squash before merge:

--- packages/clay-card/src/index.tsx
+++ packages/clay-card/src/index.tsx
@@ -3,17 +3,4 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import ClayCard from './Card';
-import {ClayCardWithHorizontal} from './CardWithHorizontal';
-import {ClayCardWithInfo} from './CardWithInfo';
-import {ClayCardWithNavigation} from './CardWithNavigation';
-import {ClayCardWithUser} from './CardWithUser';
-
-export {
-	ClayCardWithInfo,
-	ClayCardWithHorizontal,
-	ClayCardWithNavigation,
-	ClayCardWithUser,
-};
-
-export default ClayCard;
+export default () => {};
