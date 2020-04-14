2d3edd765ffe854671cd377af7efa290a37491f9
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2020-04-14 15:14:05 -0700

    will squash before merge:

--- scripts/check-size/index.js
+++ scripts/check-size/index.js
@@ -75,6 +75,7 @@ function main() {
 
 			// eslint-disable-next-line
 			console.log(table);
+			console.log(Math.abs(totalDiff) > THRESHHOLD);
 
 			if (Math.abs(totalDiff) > THRESHHOLD) {
 				process.exit(1);
